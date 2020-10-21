2671e996712473c42432dd63fe7002bef27bd3cd
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-10-21 19:20:37 +0800

    update

--- 4. standard input.py
+++ 4. standard input.py
@@ -1,3 +1,4 @@
 name = input('tell me your name, punk: ')
 age = input('...and your age: ')
 print('hello, ' + age + ' years old ' + name)
+
---
+++ 5. calc the area of a circle.py
@@ -0,0 +1,3 @@
+radius = input('Enter the radius of your circle (m): ')
+area = 3.142 * int(radius)**2
+print('the area of your circle is: ', area)
