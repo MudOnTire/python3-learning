7216594786717b612cc9e7b5342764c7635690e1
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-10-21 20:07:38 +0800

    update

---
+++ 9. for loops.py
@@ -0,0 +1,15 @@
+ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
+
+# for ninja in ninjas:
+#     print(ninja)
+
+
+# for ninja in ninjas[0:2]:
+#     print(ninja)
+
+for ninja in ninjas:
+    if ninja == 'yoshi':
+        print(f'{ninja} - blakc belt')
+        break
+    else:
+        print(ninja)
