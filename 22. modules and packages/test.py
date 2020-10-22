33125eb23b22f9132cc3439e1baf63d2f4d008f6
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-10-22 10:28:40 +0800

    update

---
+++ 22. modules and packages/test.py
@@ -0,0 +1,8 @@
+from planet import Planet
+
+naboo = Planet('Naboo', 300000, 8, 'Naboo System')
+
+print(f'Name is {naboo.name}')
+print(f'Radius is {naboo.radius}')
+print(f'Gravity is {naboo.gravity}')
+print(f'System is {naboo.system}')
