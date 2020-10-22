22d9f63403daa1a189d713abdb05502823bbed42
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-10-22 11:18:51 +0800

    update

--- 22. modules and packages/classes.py
+++ 22. modules and packages/classes.py
@@ -4,6 +4,6 @@ from space.calc import planet_mass, planet_vol
 naboo = Planet('Naboo', 300000, 8, 'Naboo System')
 
 naboo_mass = planet_mass(naboo.gravity, naboo.radius)
-naboo_vol = planet_mass(naboo.radius)
+naboo_vol = planet_vol(naboo.radius)
 
 print(f'{naboo.name} has a mass of {naboo_mass} and a volume of {naboo_vol}')
---
+++ 23. bar tab calculator.py/test.py
@@ -0,0 +1,8 @@
+from bar_tab import Tab
+
+table1 = Tab()
+table1.add('soft_drink')
+table1.add('chicken')
+table1.add('desert')
+
+table1.print_bill(10, 10)
