3550da6b9d281e98f7849f9c3bd878b8fc38d80b
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-10-22 09:46:49 +0800

    update

---
+++ 17. sets and sorting.py
@@ -0,0 +1,17 @@
+nums = [1, 23, 3, 4, 3, 51, 23, 5, 341, 23]
+print(sorted(nums))
+
+names = ['bruce', 'crixus', 'spartacus',
+         'jason', 'bruce', 'gannecus', 'MudOnTire']
+print(sorted(names))
+
+print(set(nums))
+print(set(names))
+
+ninjas = {
+    'ryu': 'black',
+    'yoshi': 'red',
+    'crystal': 'black'
+}
+
+print(set(ninjas.values()))
